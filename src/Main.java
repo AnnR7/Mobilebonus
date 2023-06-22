2de8c1814f8a9dcf7fdc88ada62817fe9e5564ce
AnnR7 <134073278+AnnR7@users.noreply.github.com>
2023-06-22 21:15:57 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         int check = 580; //начальный счет
         int refill = 1780; //сумма пополнения
-        int final_score = check + refill; //итоговый счет
+        int finalScore = check + refill; //итоговый счет
 
         int bonus = 0;
 
         if (refill >= 1001) {
             bonus = refill / 100;
         }
-        System.out.println((final_score) + " Итоговый счет");
+        System.out.println((finalScore) + " Итоговый счет");
         System.out.println((bonus) + " Бонусные рубли");
     }
 }
